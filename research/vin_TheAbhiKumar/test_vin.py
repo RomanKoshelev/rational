0d03790505ef518ed9105d225cac3612b48244af
Roman Koshelev <roman.koshelev@groupdocs.com>
2017-03-12 15:08:57 +0300

    refactoring vin_TheAbhKumar

--- research/vin_TheAbhiKumar/test_vin.py
+++ research/vin_TheAbhiKumar/test_vin.py
@@ -3,7 +3,7 @@ import unittest
 from common.timers.event_timer import EventTimer
 from research.vin_TheAbhiKumar.config import config
 from research.vin_TheAbhiKumar.utils.logger import VinTrainLogger
-from research.vin_TheAbhiKumar.vin_algorithm.train import train
+from research.vin_TheAbhiKumar.vin_algorithm.vin_algorithm import VinAlgorithm
 
 
 class TestVin(unittest.TestCase):
@@ -11,7 +11,8 @@ class TestVin(unittest.TestCase):
     # noinspection PyMethodMayBeStatic
     def run_experiment(self, cfg):
         with EventTimer('algorithm.train_epoch'), VinTrainLogger():
-            train()
+            alg = VinAlgorithm()
+            alg.train()
 
     def test_8x8(self):
         self.run_experiment(config)
--- research/vin_TheAbhiKumar/vin_algorithm/utils.py
+++ research/vin_TheAbhiKumar/vin_algorithm/utils.py
@@ -1,22 +1,4 @@
-import numpy as np
 import tensorflow as tf
-
-
-# helper methods to print nice table (taken from CGT code)
-def fmt_item(x, l):
-    if isinstance(x, np.ndarray):
-        assert x.ndim == 0
-        x = x.item()
-    if isinstance(x, float):
-        rep = "%5g" % x
-    else:
-        rep = str(x)
-    return " " * (l - len(rep)) + rep
-
-
-def fmt_row(width, row):
-    out = " | ".join(fmt_item(x, width) for x in row)
-    return out
 
 
 def flipkernel(kern):
